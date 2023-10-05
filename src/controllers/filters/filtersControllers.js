db510a54bcd7cc83fda5c9ccaf223c71393a8381
Vladimir Peretiatko <djbob2000@gmail.com>
2023-10-06 00:47:01 +0300

    feat: add filters controller

---
+++ src/controllers/filters/filtersControllers.js
@@ -0,0 +1,36 @@
+const catchAsync = require("../../helpers/catchAsync");
+const db = require("../../models");
+
+const getFiltersController = catchAsync(async (req, res) => {
+  const { query } = req;
+
+  const eventTypesAll = await db.EventTypes.findAll({
+    attributes: ["event_type"],
+    where: {
+      locale: query.locale,
+    },
+  });
+  const eventTypes = [...new Set(eventTypesAll.map((item) => item.event_type))];
+
+  const EventCitiesAll = await db.EventAddress.findAll({
+    attributes: ["city"],
+    where: {
+      locale: query.locale,
+    },
+  });
+  const eventCities = [...new Set(EventCitiesAll.map((item) => item.city))];
+
+  const EventDatesAll = await db.Events.findAll({
+    attributes: ["date_time"],
+    where: {
+      locale: query.locale,
+    },
+  });
+  const eventDates = [...new Set(EventDatesAll.map((item) => item.date_time))];
+
+  res.json({ eventCities, eventDates, eventTypes });
+});
+
+module.exports = {
+  getFiltersController,
+};
