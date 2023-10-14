6f4071bfe17ecebe61e72cd716f37509edd8bf70
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-10-14 21:15:08 +0300

    fix: fixes controllers

--- src/controllers/events/createEventController.js
+++ src/controllers/events/createEventController.js
@@ -1,65 +1,5 @@
-const { Op, literal } = require("sequelize");
 const catchAsync = require("../../helpers/catchAsync");
 const db = require("../../models");
-
-const getEventsController = catchAsync(async (req, res) => {
-  const { query } = req;
-
-  const localeQuery = query.locale;
-  const citiesQuery = query.city ? query.city.split(",") : [];
-  const dates = query.date ? query.date.split(",") : [];
-  const dateQuery = dates.map((dateString) => {
-    return literal(`DATE(date_time) = DATE('${dateString}')`);
-  });
-  const eventTypesQuery = query.eventType ? query.eventType.split(",") : [];
-
-  const events = await db.Events.findAll({
-    where: {
-      ...(localeQuery && {
-        locale: localeQuery,
-      }),
-      ...(dateQuery.length > 0 && {
-        date_time: {
-          [Op.or]: dateQuery,
-        },
-      }),
-    },
-    include: [
-      {
-        model: db.EventAddress,
-        ...(citiesQuery.length > 0 && {
-          where: {
-            ...(localeQuery && {
-              locale: localeQuery,
-            }),
-            city: {
-              [Op.in]: citiesQuery,
-            },
-          },
-        }),
-      },
-      {
-        model: db.EventTypes,
-        through: {
-          model: db.EventTypeRelationships,
-          attributes: [],
-        },
-        ...(eventTypesQuery.length > 0 && {
-          where: {
-            ...(localeQuery && {
-              locale: localeQuery,
-            }),
-            event_type: {
-              [Op.in]: eventTypesQuery,
-            },
-          },
-        }),
-      },
-    ],
-  });
-
-  res.status(200).json(events);
-});
 
 const createEventController = catchAsync(async (req, res) => {
   const {
@@ -135,7 +75,4 @@ const createEventController = catchAsync(async (req, res) => {
   res.status(201).json(newEvent);
 });
 
-module.exports = {
-  getEventsController,
-  createEventController,
-};
+module.exports = createEventController;
--- src/controllers/filters/filtersControllers.js
+++ src/controllers/filters/filtersControllers.js
@@ -35,6 +35,4 @@ const getFiltersController = catchAsync(async (req, res) => {
   res.json({ eventCities, eventDates, eventTypes });
 });
 
-module.exports = {
-  getFiltersController,
-};
+module.exports = getFiltersController;
