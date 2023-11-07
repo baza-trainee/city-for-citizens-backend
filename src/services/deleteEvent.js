f5f84696de459eb0a6e03e778909a4994b9eeffa
Volodymyr Peretiatko <djbob2000@gmail.com>
2023-11-08 00:38:31 +0200

    fix: add delete image

--- src/services/deleteEvent.js
+++ src/services/deleteEvent.js
@@ -1,10 +1,16 @@
 const db = require('../models');
+const deleteImage = require('./deleteImage');
 
 async function deleteEvent(eventId) {
   const event = await db.Events.findByPk(eventId);
 
   if (!event) {
     throw new Error('Event not found');
+  }
+
+  const successfullyDeletedImage = deleteImage(event.eventImage, {});
+  if (!successfullyDeletedImage) {
+    throw new Error('Image not found or could not be deleted');
   }
 
   await db.EventTypeRelationships.destroy({
